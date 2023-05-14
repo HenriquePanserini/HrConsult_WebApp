7abc3104ba2fff455eb9bd2fae618478cef26109
Henrique Panserini <henrique.panserini@gmail.com>
2023-05-14 17:42:07 -0300

    Adicionado estrutura de cards

---
+++ Asse-consult/src/app/model/empresa-dados-cards.model.ts
@@ -0,0 +1,14 @@
+export const card_info_emp = [
+    {
+        'nome':'JTK',
+        'cnpj':'45.835.451/0001-87'
+    },
+    {
+        'nome':'JTK',
+        'cnpj':'45.835.451/0001-87'
+    },
+    {
+        'nome':'JTK',
+        'cnpj':'45.835.451/0001-87'
+    }
+]
